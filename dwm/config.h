27bc8217507202654fd730d90d086bb0b43689e7
Soliprem <franci.solidoro@gmail.com>
2022-06-09 18:05:04 +0200

    changes

--- dwm/config.h
+++ dwm/config.h
@@ -12,7 +12,7 @@ static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = {"monospace:size=10"};
+static char font[]            = {"SauceCodePro Nerd Font:style=regular:pixelsize=14:antialias=true:autohint=true"};
 static char dmenufont[]       = "monospace:size=10";
 static const char *fonts[]          = { font, "fontawesome-regular:size=10" };
 static char normbgcolor[]           = "#282828";
